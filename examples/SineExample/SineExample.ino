af8a2fe42b14562037f2d964a9dd554cb926afd3
Agrimagsrl <web@agrimag.it>
2020-01-29 18:17:35 +0100

    fix library.properties

--- examples/SineExample/SineExample.ino
+++ examples/SineExample/SineExample.ino
@@ -1,14 +1,16 @@
 #include <EloquentTinyML.h>
+// sine_model.h contains the array you exported from the previous step with xxd or tinymlgen
 #include "sine_model.h"
 
 #define NUMBER_OF_INPUTS 1
 #define NUMBER_OF_OUTPUTS 1
+// in future projects you may need to tweek this value: it's a trial and error process
 #define TENSOR_ARENA_SIZE 2*1024
 
 Eloquent::TinyML::TinyML<
         NUMBER_OF_INPUTS,
         NUMBER_OF_OUTPUTS,
-        TENSOR_ARENA_SIZE> ml(sine_model_quantized_tflite);
+        TENSOR_ARENA_SIZE> ml(sine_model);
 
 
 void setup() {
@@ -16,6 +18,7 @@ void setup() {
 }
 
 void loop() {
+    // pick up a random x and predict its sine
     float x = 3.14 * random(100) / 100;
     float y = sin(x);
     float input[1] = { x };
